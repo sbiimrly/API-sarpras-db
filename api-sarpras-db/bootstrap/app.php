f6ae5e5343cbf1dc0397bfd4bb0067bde097c39e
sbiimrly <imodaysbim07@gmail.com>
2025-12-10 13:35:59 +0700

    [+] Add RateLimiter

--- api-sarpras-db/bootstrap/app.php
+++ api-sarpras-db/bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,7 +13,16 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
-    )
+            then: function () {
+        RateLimiter::for('admin-login', function (Request $request) {
+            $key = 'login:' . $request->ip() . ':' . strtolower($request->email);
+            return Limit::perMinute(5)->by($key)->response(function () {
+                return response()->json([
+                    'message' => 'Terlalu banyak percobaan login. Coba lagi dalam 10 menit.'
+                ], 429);
+            });
+        });
+    })
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
--- api-sarpras-db/routes/api.php
+++ api-sarpras-db/routes/api.php
@@ -8,12 +8,23 @@ use App\Http\Controllers\Auth\AuthController;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
+// Allow preflight OPTIONS requests
+Route::options('/{any}', function () {
+    return response()->json([], 200);
+})->where('any', '.*');
+
 Route::get('/test-api-check', function () {
     return 'API Loaded';
 });
 
-// COBA
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+// Authentication Routes
+Route::post('/login', [AuthController::class, 'login'])
+    ->middleware('throttle:admin-login');
+
+Route::post('/logout', [AuthController::class, 'logout'])
+->middleware('auth:sanctum');
+
+Route::get('/user', [AuthController::class, 'user'])
+->middleware('auth:sanctum');
+
 
